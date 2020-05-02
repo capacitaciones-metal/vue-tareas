4a1a77f3aaa67c9c5f4ecb84d6460393d25f3d7c
cincarnato <cristian.incarnato@sondeos.com.ar>
2020-05-02 10:29:41 -0300

    1er implementacion

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,28 @@
 <template>
 
-  <div id="app">
-    <tareas></tareas>
-  </div>
+    <v-app>
+        <v-app-bar app color="primary" dark>
+            <v-toolbar-title>Gestion de Tareas</v-toolbar-title>
+            <v-spacer></v-spacer>
+            <v-btn icon target="_blank" href="http://fullstack-extraordinary.com/curso-vue-vuex-vuetify/">
+                <v-icon>mdi-link</v-icon>
+            </v-btn>
+        </v-app-bar>
+
+        <v-content class="grey lighten-3">
+            <tareas></tareas>
+        </v-content>
+    </v-app>
 
 </template>
 
 <script>
-import Tareas from "./components/Tareas";
+    import Tareas from "./components/Tareas";
 
-export default {
-  name: 'App',
-  components:{Tareas},
-}
+    export default {
+        name: 'App',
+        components: {Tareas},
+    }
 </script>
 
 <style>
--- src/components/Tareas.vue
+++ src/components/Tareas.vue
@@ -1,18 +1,42 @@
 <template>
-    <div>
-        <h2>Gestion de tareas</h2>
+    <v-container>
+        <v-row>
+            <v-col cols="8">
+                <v-card>
+                    <v-card-text>
+                        <tareas-nueva></tareas-nueva>
+                    </v-card-text>
+                </v-card>
 
-        <span>Vigentes: {{obtenerTareasvigentes}}</span> - <span>Eliminadas: {{contadorEliminadas}} </span> - <span>Totales: {{obtenerTareasTotales}}</span>
-        <br><br>
-        <tareas-nueva ></tareas-nueva>
-        <br>
-        <tareas-filtrar></tareas-filtrar>
+                <v-card class="mt-3">
+                    <v-card-text>
+                        <tareas-filtrar></tareas-filtrar>
+                    </v-card-text>
+                </v-card>
 
-        <div v-if="cargando" class="cargando"><br><br>Cargando... </div>
-        <TareasListar v-else />
+                <v-card class="mt-3" >
+                    <v-card-text>
+                        <div v-if="cargando" class="cargando"><br><br>Cargando...</div>
+                        <TareasListar v-else/>
+                    </v-card-text>
+                </v-card>
+            </v-col>
+
+            <v-col cols="4">
+                <v-card>
+                    <v-card-text>
+                        <span>Vigentes: {{obtenerTareasvigentes}}</span> -
+                        <span>Eliminadas: {{contadorEliminadas}} </span> -
+                        <span>Totales: {{obtenerTareasTotales}}</span>
+                    </v-card-text>
+                </v-card>
+            </v-col>
+        </v-row>
+
 
         <tareas-notificador :notificar="notificar"></tareas-notificador>
-    </div>
+    </v-container>
+
 </template>
 
 <script>
@@ -26,10 +50,10 @@
         name: 'Tareas',
         components: {TareasListar, TareasNueva, TareasFiltrar, TareasNotificador},
         mounted() {
-           this.cargarTareas()
+            this.cargarTareas()
         },
         computed: {
-            ...mapState(['cargando','tareas', 'notificar','contadorEliminadas']), //this.cargando
+            ...mapState(['cargando', 'tareas', 'notificar', 'contadorEliminadas']), //this.cargando
             obtenerTareasvigentes() {
                 return this.tareas.length
             },
